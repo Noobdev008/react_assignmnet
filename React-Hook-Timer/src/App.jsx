ec68107ef5b0647dee4de849c9383c43e50b0ac8
Shubham Srivastava <86356397+Noobdev008@users.noreply.github.com>
2022-01-06 14:25:55 +0530

    Add files via upload

---
+++ React-Hook-Timer/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Timer } from './components/Timer';
+
+function App() {
+    return (
+        <div className='mainBox'>
+
+            <Timer />
+
+        </div>
+    );
+}
+
+export default App;
